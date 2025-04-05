01061885a1227f07cdd1b311612b56ba5bc4e53b
Rahul Kurmalkar <therahulkurmalkar@gmail.com>
2025-04-05 19:54:15 +0530

    changes_index.js

--- backend-seat-booking/src/index.js
+++ backend-seat-booking/src/index.js
@@ -7,7 +7,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/v1', router)
+app.use('/', (req, res)=>{
+    return res.json({message: "Backend is running like a butter"});
+})
+app.use('/api/v1', router);
+
 
 app.listen(process.env.PORT || 3000, ()=> {
     console.log(`Server is running on port: ${process.env.PORT || 3000}`);
