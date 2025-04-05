72ad4405f8fffdaaeea56d8e5aafff86b6464930
Rahul Kurmalkar <therahulkurmalkar@gmail.com>
2025-04-05 19:24:34 +0530

    changes_port

--- backend-seat-booking/src/index.js
+++ backend-seat-booking/src/index.js
@@ -10,5 +10,5 @@ app.use(express.json());
 app.use('/api/v1', router)
 
 app.listen(process.env.PORT || 3000, ()=> {
-    console.log(`Server is running on port: ${process.env.PORT }`);
+    console.log(`Server is running on port: ${process.env.PORT || 3000}`);
 })
