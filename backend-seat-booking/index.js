3db4b3ed72b943b698a32bdecd28fd0063fc0f64
Rahul Kurmalkar <therahulkurmalkar@gmail.com>
2025-04-07 04:12:19 +0530

    changes_index.js

--- backend-seat-booking/index.js
+++ backend-seat-booking/index.js
@@ -7,10 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/v1', router);
 app.use('/', (req, res)=>{
     return res.json({message: "Backend is running like a butter"});
 })
-app.use('/api/v1', router);
 
 
 app.listen(process.env.PORT || 3000, ()=> {
